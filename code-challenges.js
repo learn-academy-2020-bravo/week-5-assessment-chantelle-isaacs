e52e358d2410cefc9ada64ec1d13a6264b8ade26
Chantelle Isaacs <chantelle616@gmail.com>
2020-05-31 23:11:15 -0700

    completed week 5 assessment... but it was a rough weekend to concentrate on this

--- code-challenges.js
+++ code-challenges.js
@@ -7,18 +7,54 @@ var secretCodeWord1 = "lackadaisical"
 var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
+//make a function that takes in a string
 
+const coder = (string) => {
+    //split the string into an array
+    let array = string.split("")
+    //map over the array and evaluate each value to return the coded character
+    let replacement = array.map(val => {
+        if (val === "a"){
+            return "4"
+        } else if (val === "e"){
+            return "3"
+        } else if (val === "i"){
+            return "1"
+        } else if (val === "o"){
+            return "0"
+        } else {
+            return val
+        }
+    //return replacement joined back into a string
+    })
+    return replacement.join("")
+}
 
-
+console.log(coder(secretCodeWord1))
+console.log(coder(secretCodeWord2))
 
 // --------------------2) Create a function that takes in an array and returns all the words that have the letter a in them.
 
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
 
+//create a function that takes in an array
+const aFruityFinder = (array) => {
+    //create a new array to push a-fruits into
+    let newArray = []
+    //map over the array
+    array.map(value => {
+        //if the string-value lowercased contains, an "a"
+        if (value.toLowerCase().includes("a")){
+            //push the value into the new array
+            newArray.push(value)
+        }
+    })
+    //return the new array of "a" containers
+    return newArray
+}
 
-
-
+console.log(aFruityFinder(arrayOfWords))
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is one pair and one three of a kind.
 
@@ -28,3 +64,26 @@ var hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
+
+//create a function that takes in an array
+const fullHouse = (array) => {
+    //sort the array from smallest to greatest
+    sorted = array.sort((a,b)=>a-b)
+    //here's where the magic happens! looking at a sorted array of 5 nums, if numbers 1&2 are the same AND numbers 3&5 are the same (we don't have to evaluate 4 if it's sorted) then it's a full house! 
+    if (sorted[0]===sorted[1] && sorted[2]===sorted[4]){
+        return true
+    //take that logic, flip it and reverse it (1&3 and 4&5)
+    } else if (sorted[0]===sorted[2]&& sorted[3]===sorted[4]){
+        return true
+    //otherwise bob can't be your uncle and it's not a full house. You lose bud. 
+    } else {
+        return false
+    }
+}
+console.log(fullHouse(hand1))
+console.log(fullHouse(hand2))
+console.log(fullHouse(hand3))
+//just wanted to make quadrupily sure it worked so I made my own array of alternating numbers
+let t= [9,5,9,5,9]
+console.log(fullHouse(t))
+
--- rails-questions.md
+++ rails-questions.md
@@ -1,27 +1,38 @@
 # ASSESSMENT 5: Rails Practical Questions
 
 1. What is the command to create a new Rails application with a Postgres database?
-
+    $ rails new app_name -d postgresql -T
+    obvs change app_name to whatever your heart desires ;) 
 
 
 2. Explain each section of the following route:  get '/library/:id' => 'book#show'
 
-get -
-library -
-:id -
-book -
-show -
+get -  the HTTP verb we are sending to the server via the URL
+library - this is the url
+:id - the param that will be input by the user
+book - name of the controller
+show - name of the method found in the controller
 
 3. Oops, I forgot to add a foreign key to my model. Describe the steps to remedy this mistake.
 
-
+We're gonna do a migration! We need to modify the database to include a column for our foriegn keys
+    First, we need to generate a migration : $ rails generate migration add_foreign_key_to_table
+    Then, in our text editor, we find the migration file and add:
+        $ def change
+        add_column :table_name, :foreign_key, :integer
+        end
+    Then we need to make sure we create the association between the two models
 
 
 
 4. What is the command to generate a Rails model for a person table with columns named shirt, pants, and shoes?
 
+$ rails generate model Person shirt:string pants:string shoes:string
+$ rails db:migrate
 
 
 
 
 5. What is the naming convention for generating a Rails model? What is the naming convention for the table that is generated with this command?
+for generating a Rails model - the model name is PascalCase and singular. 
+for the table that is generated with this command - snake_case
